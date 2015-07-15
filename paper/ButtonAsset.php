dab9ca469eeafe1b154a0b170517587da41c3341
p0larbeer <p0larbeer@mail.ru>
2015-07-15 14:39:53 +0300

    added paper button asset

---
+++ paper/ButtonAsset.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 15.07.15
+ * Time: 14:38
+ */
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class ButtonAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-button/paper-button.html'
+    ];
+}
