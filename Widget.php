503e50c5425473b82960beeb867af0ada97341da
p0larbeer <p0larbeer@mail.ru>
2015-07-15 14:37:35 +0300

    added base Widget

---
+++ Widget.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 15.07.15
+ * Time: 14:37
+ */
+
+namespace p0larbeer\yii\polymer;
+
+
+class Widget extends \yii\base\Widget
+{
+    /**
+     * @var array the HTML attributes for the widget container tag.
+     * @see \yii\helpers\Html::renderTagAttributes() for details on how attributes are being rendered.
+     */
+    public $options = [];
+}
