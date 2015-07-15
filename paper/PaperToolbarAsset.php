9b2888558f06d0515541cc0ca1910ff98f85ed2f
p0larbeer <p0larbeer@mail.ru>
2015-07-16 00:27:37 +0300

    added PaperToolbarAsset

---
+++ paper/PaperToolbarAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 16.07.15
+ * Time: 0:26
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class PaperToolbarAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-toolbar/paper-toolbar.html'
+    ];
+}
