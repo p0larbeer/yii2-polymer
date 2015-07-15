7008307aa333abb001570b67ce2afb55900d3e30
p0larbeer <p0larbeer@mail.ru>
2015-07-16 01:29:31 +0300

    added MaterialAsset

---
+++ paper/MaterialAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 16.07.15
+ * Time: 1:28
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class MaterialAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-material/paper-material.html'
+    ];
+}
