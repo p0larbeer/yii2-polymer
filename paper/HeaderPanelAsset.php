8cc4146dba9a1e06ddb79760ff8546902ef1ba1f
p0larbeer <p0larbeer@mail.ru>
2015-07-16 00:59:24 +0300

    added PaperHeaderAsset

---
+++ paper/HeaderPanelAsset.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 16.07.15
+ * Time: 0:57
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class HeaderPanelAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-header-panel/paper-header-panel.html'
+    ];
+}
+
