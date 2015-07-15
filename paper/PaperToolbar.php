05d122f347edbaa7592ca1634013527c0973e1af
p0larbeer <p0larbeer@mail.ru>
2015-07-16 00:32:40 +0300

    added PaperToolbar widget

---
+++ paper/PaperToolbar.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 16.07.15
+ * Time: 0:27
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use yii\helpers\Html;
+use p0larbeer\yii\polymer\Widget;
+
+class PaperToolbar extends Widget
+{
+    /**
+     * @var string the tag to use to render the button
+     */
+    public $tagName = 'paper-toolbar';
+    /**
+     * @var string the dialog content
+     */
+    public $content;
+    /**
+     * @var boolean whether the content should be HTML-encoded.
+     */
+    public $encodeContent = true;
+
+    public function init()
+    {
+        $view = $this->getView();
+        PaperToolbarAsset::register($view);
+    }
+    public function run()
+    {
+        return Html::tag(
+            $this->tagName,
+            $this->encodeContent ? Html::encode($this->content) : $this->content,
+            $this->options
+        );
+    }
+}
