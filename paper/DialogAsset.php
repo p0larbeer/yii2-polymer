df9a473e9e0388eef8fe899953096d8757b8fca2
p0larbeer <p0larbeer@mail.ru>
2015-07-15 22:31:34 +0300

    added DialogAsset

---
+++ paper/DialogAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 15.07.15
+ * Time: 16:29
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class DialogAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-dialog/paper-dialog.html'
+    ];
+}
