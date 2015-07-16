8b965a3beebc1d7b60676d256c8f39d5a067bb2c
p0larbeer <p0larbeer@mail.ru>
2015-07-16 15:25:34 +0300

    add DrawerPanelAsset

---
+++ paper/DrawerPanel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 16.07.15
+ * Time: 15:24
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class DrawerPanel extends PolymerAsset
+{
+    public $href = [
+        'paper-drawer-panel/paper-drawer-panel.html'
+    ];
+}
