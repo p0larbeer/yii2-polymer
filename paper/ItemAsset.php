a5abd2979e8e483e28d7d9d3528d232f6e489398
p0larbeer <p0larbeer@mail.ru>
2015-07-16 14:56:05 +0300

    added ItemAsset

---
+++ paper/ItemAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 16.07.15
+ * Time: 14:54
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class ItemAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-item/paper-item.html'
+    ];
+}
