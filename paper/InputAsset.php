b7ecada964a23aea66d0ae5a16933a19f120b839
p0larbeer <p0larbeer@mail.ru>
2015-07-16 23:36:47 +0300

    add InputAsset

---
+++ paper/InputAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 16.07.15
+ * Time: 23:26
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class InputAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-input/paper-input.html'
+    ];
+}
