19fa2457ed9ff39218e80fe4f1da8efd38cdc933
p0larbeer <p0larbeer@mail.ru>
2015-07-16 15:55:47 +0300

    add IconIconsAsset

---
+++ iron/ironIconsAsset.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 15.07.15
+ * Time: 14:38
+ *
+ * A set of icons for use with iron-icon
+ */
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class IronIconsAsset extends PolymerAsset
+{
+    public $href = [
+        'iron-icons/iron-icons.html'
+    ];
+}
