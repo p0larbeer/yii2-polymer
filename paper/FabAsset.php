48ad7675434fd6ddd8f520af5eb05291d63f31bc
p0larbeer <p0larbeer@mail.ru>
2015-07-16 15:38:19 +0300

    add FabAsset

---
+++ paper/FabAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 16.07.15
+ * Time: 15:37
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class FabAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-fab/paper-fab.html'
+    ];
+}
