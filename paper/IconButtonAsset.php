d508011f22c49df447d624ab1858a98b93f4f90d
p0larbeer <p0larbeer@mail.ru>
2015-07-16 15:45:13 +0300

    add IconButtonAsset

---
+++ paper/IconButtonAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 16.07.15
+ * Time: 15:37
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class IconButtonAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-icon-button/paper-icon-button.html'
+    ];
+}
