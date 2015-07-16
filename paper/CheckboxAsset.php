ea7285e7ee1f22f6aada51a8171d7e5895a00f6c
p0larbeer <p0larbeer@mail.ru>
2015-07-16 15:02:51 +0300

    added CheckboxAsset

---
+++ paper/CheckboxAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 16.07.15
+ * Time: 15:01
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class CheckboxAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-checkbox/paper-checkbox.html'
+    ];
+}
