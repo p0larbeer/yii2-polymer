40a9191f9473ff1adeec6e21b5941a2eb2722589
p0larbeer <p0larbeer@mail.ru>
2015-07-22 16:26:40 +0300

    add MenuAsset

---
+++ paper/MenuAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 22.07.15
+ * Time: 16:25
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class MenuAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-menu/paper-menu.html'
+    ];
+}
