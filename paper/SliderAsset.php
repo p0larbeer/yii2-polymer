10030716c3b86c6a2d11b1723106ec11e1224acb
p0larbeer <p0larbeer@mail.ru>
2015-07-22 15:20:01 +0300

    add SliderAsset

---
+++ paper/SliderAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 22.07.15
+ * Time: 15:19
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class SliderAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-slider/paper-slider.html'
+    ];
+}
