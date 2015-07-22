57ea6ef5a9681995d05cfcdccc048e7836964cf6
p0larbeer <p0larbeer@mail.ru>
2015-07-22 16:52:15 +0300

    add PagesAsset

---
+++ iron/PagesAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 22.07.15
+ * Time: 16:51
+ */
+
+namespace p0larbeer\yii\polymer\iron;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class PagesAsset extends PolymerAsset
+{
+    public $href = [
+        'iron-pages/iron-pages.html'
+    ];
+}
