dd836dba99370ad200749df548a3006b2b85494e
p0larbeer <p0larbeer@mail.ru>
2015-07-22 14:49:42 +0300

    add RadioGroupAsset

---
+++ paper/RadioGroupAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 22.07.15
+ * Time: 14:48
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class RadioGroupAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-radio-group/paper-radio-group.html'
+    ];
+}
