9f7a554d6b70d1b43aa332c00ddcebe48326e53e
p0larbeer <p0larbeer@mail.ru>
2015-07-22 10:09:56 +0300

    add ToggleButtonAsset

---
+++ paper/ToggleButtonAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 22.07.15
+ * Time: 10:08
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class ToggleButtonAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-toggle-button/paper-toggle-button.html'
+    ];
+}
