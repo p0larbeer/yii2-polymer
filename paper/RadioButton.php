0c2862672e1f1e754e606565b807819913a3cf87
p0larbeer <p0larbeer@mail.ru>
2015-07-22 14:47:27 +0300

    add RadioButton widget

---
+++ paper/RadioButton.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 22.07.15
+ * Time: 14:45
+ *
+ * Documentation:
+ * paper-radio-button is a button that can be either checked or unchecked.
+ * User can tap the radio button to check or uncheck it.
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use yii\helpers\Html;
+use p0larbeer\yii\polymer\Widget;
+
+class RadioButton extends Widget
+{
+    /**
+     * @var string the tag to use to render the element
+     */
+    public $tagName = 'paper-radio-button';
+    /**
+     * @var string the checkbox label
+     */
+    public $label;
+    /**
+     * @var boolean whether the label should be HTML-encoded.
+     */
+    public $encodeLabel = true;
+    /**
+     * @var boolean Gets or sets the state, true is checked and false is unchecked.
+     */
+    public $checked = false;
+    /**
+     * @var boolean If true, the button toggles the active state with each tap or press of the spacebar.
+     */
+    public $toggles = false;
+
+    public function init()
+    {
+        $view = $this->getView();
+        CheckboxAsset::register($view);
+    }
+    public function run()
+    {
+        $this->options['checked'] = $this->checked;
+        $this->options['toggles'] = $this->toggles;
+        return Html::tag(
+            $this->tagName,
+            $this->encodeLabel ? Html::encode($this->label) : $this->label,
+            $this->options
+        );
+    }
+}
