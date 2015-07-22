41a2f2fb6073a9cecb2865c9bb06f18b6e9f2c91
p0larbeer <p0larbeer@mail.ru>
2015-07-22 12:04:59 +0300

    add ToastAsset

---
+++ paper/ToastAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 22.07.15
+ * Time: 12:03
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class ToastAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-toast/paper-toast.html'
+    ];
+}
