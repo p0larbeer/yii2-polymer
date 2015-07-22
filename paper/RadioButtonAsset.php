1f355e652470e3ffa62f96dcc999ac801c25f8cf
p0larbeer <p0larbeer@mail.ru>
2015-07-22 14:45:04 +0300

    add RadioButtonAsset

---
+++ paper/RadioButtonAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 22.07.15
+ * Time: 14:44
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class RadioButtonAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-radio-button/paper-radio-buton.html'
+    ];
+}
