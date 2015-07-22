f88afca11e4de2b754c0aaa3e0afffa70343b136
p0larbeer <p0larbeer@mail.ru>
2015-07-22 12:19:15 +0300

    add TabAsset

---
+++ paper/TabAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 22.07.15
+ * Time: 12:17
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class TabAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-tabs/paper-tab.html'
+    ];
+}
