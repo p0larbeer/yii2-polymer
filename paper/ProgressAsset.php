093b4f429823eb0076f2d8c5df844d77e28c6e8d
p0larbeer <p0larbeer@mail.ru>
2015-07-22 14:59:14 +0300

    add ProgressAsset

---
+++ paper/ProgressAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 22.07.15
+ * Time: 14:58
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class ProgressAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-progress/paper-radio-progress.html'
+    ];
+}
