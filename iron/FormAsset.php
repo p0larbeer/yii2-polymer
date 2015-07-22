cd55f72ed1150c6b17066d290d8706ca48a33b6d
p0larbeer <p0larbeer@mail.ru>
2015-07-22 18:54:43 +0300

    add FormAsset

---
+++ iron/FormAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 22.07.15
+ * Time: 18:53
+ */
+
+namespace p0larbeer\yii\polymer\iron;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class FormAsset extends PolymerAsset
+{
+    public $href = [
+        'iron-form/iron-form.html'
+    ];
+}
