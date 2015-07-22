97f5320e60487b7a26e0308790df5f24be30f27d
p0larbeer <p0larbeer@mail.ru>
2015-07-22 15:23:04 +0300

    add SpinnerAsset

---
+++ paper/SpinnerAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 22.07.15
+ * Time: 15:22
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class SpinnerAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-spinner/paper-spinner.html'
+    ];
+}
