ff1457db4c671abfb954ba677b95e629fd38b668
p0larbeer <p0larbeer@mail.ru>
2015-07-25 13:04:33 +0300

    add VaadinAsset

---
+++ vaadin/VaadinAsset.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 25.07.15
+ * Time: 13:02
+ */
+
+namespace p0larbeer\yii\polymer\vaadin;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class VaadinAsset extends  PolymerAsset
+{
+    public $href = [
+        'vaadin-components/vaadin-components.html'
+    ];
+}
