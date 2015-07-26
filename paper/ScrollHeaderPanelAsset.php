086b9b99d7a6aceb67a79e3dc40e856bc3fea583
p0larbeer <p0larbeer@mail.ru>
2015-07-26 11:23:40 +0300

    add ScrollHeaderPanelAsset

---
+++ paper/ScrollHeaderPanelAsset.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: p0larbeer
+ * Date: 26.07.15
+ * Time: 11:22
+ */
+
+namespace p0larbeer\yii\polymer\paper;
+
+use p0larbeer\yii\polymer\PolymerAsset;
+
+class ScrollHeaderPanelAsset extends PolymerAsset
+{
+    public $href = [
+        'paper-scroll-header-panel/paper-scroll-header-panel.html'
+    ];
+}
